1f6182ea5cc6b1697140d4801cb2f68430b71954
Eisha Kaushal <eishakaushal@MacBook-Pro.local>
2023-07-15 17:31:28 -0400

    modified

--- server/controllers/animeController.js
+++ server/controllers/animeController.js
@@ -4,15 +4,28 @@ const animeController = {
 
  
 async userFavorites(req,res,next){
-   
+   const {name} = req.body
   try{
-    const getShows= ''
+    const insertIntoFavorites= 'INSERT INTO favorites (favoriteItem) VALUES($1)'
 
-    await db.query
+    await db.query(insertIntoFavorites,[name]) ;
+     
+    const userID = req.params.userID;
 
 
+    const updateUserFavorites = 'SELECT (id) FROM favorites WHERE name = $2';
 
-  }
+    const retrieveFavoritesID = await db.query(updateUserFavorites, [name], (error,data)=>{
+      if (error){
+        console.log(error)
+      }else{
+        const insertNewUserFavorites = 'INSERT INTO userFavorites (userid,favoriteid) VALUES($3,$4)'
+
+        db.query(insertNewUserFavorites, [userID, data.ID])
+
+      }
+    })
+  }  
   catch(err){
     return next({
     log: 'Error occured at animeController.userFavorites' + err,
